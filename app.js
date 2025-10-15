221975acc6ca6f2e7354198a3203075aa301246d
DaphneSartorato <daphne.sartorato@gmail.com>
2025-10-15 16:46:49 -0300

    Api com express

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.json());
 const Routes = require('./interfaces/routes/routes.js');
 app.use('/pdf-docs', Routes);
 
-app.listen(2297, ()=>{
-    console.log("Server Iniciado na Porta 2297");
+app.listen(2205, ()=>{
+    console.log("Server Iniciado na Porta 2205");
 })
